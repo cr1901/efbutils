17d31c2177abe48f37ed4b186044ffb5e18c1fe9
William D. Jones <thor0505@comcast.net>
2023-09-07 21:11:38 -0400

    Make some mild refactors to sequencer. NFCI.

--- src/efbutils/ufm/reader/sequencer.py
+++ src/efbutils/ufm/reader/sequencer.py
@@ -1,5 +1,5 @@
 from amaranth import Signal, Module, unsigned
-from amaranth.lib.data import ArrayLayout, Struct, Union, View
+from amaranth.lib.data import Struct, Union
 from amaranth.lib.enum import IntEnum
 from amaranth.lib.wiring import Signature, In, Out, Component
 
@@ -115,10 +115,6 @@ class Sequencer(Component):
         curr_op = Signal(2)
         curr_data = Signal(6)
 
-        def next_state_if_asserted(stim, state):
-            with m.If(stim):
-                m.next = state
-
         def wb_write():
             m.d.comb += [
                 self.efb.cyc.eq(1),
@@ -132,37 +128,12 @@ class Sequencer(Component):
                 self.efb.stb.eq(1),
             ]
 
-        def wb_data_slice_ops():
-            op_view = View(ArrayLayout(8, 3), self.ctl.cmd.ops)
-
-            with m.Switch(curr_op):
-                with m.Case(0):
-                    m.d.comb += self.efb.dat_w.eq(op_view[2])
-                with m.Case(1):
-                    m.d.comb += self.efb.dat_w.eq(op_view[1])
-                with m.Case(2):
-                    m.d.comb += self.efb.dat_w.eq(op_view[0])
-                with m.Default():
-                    pass
-
-        def wb_data_slice_data():
-            data_view = View(ArrayLayout(8, 4), self.ctl.wr.data)
-
-            with m.Switch(curr_data):
-                with m.Case(0):
-                    m.d.comb += self.efb.dat_w.eq(data_view[3])
-                with m.Case(1):
-                    m.d.comb += self.efb.dat_w.eq(data_view[2])
-                with m.Case(2):
-                    m.d.comb += self.efb.dat_w.eq(data_view[1])
-                with m.Case(3):
-                    m.d.comb += self.efb.dat_w.eq(data_view[0])
-                with m.Default():
-                    pass
-
+        op_idx = (2 - curr_op).as_unsigned()
+        data_idx = (3 - curr_data).as_unsigned()
         with m.FSM() as fsm:  # noqa: F841
             with m.State("IDLE"):
-                next_state_if_asserted(self.ctl.req, "WB_ENABLE_1")
+                with m.If(self.ctl.req):
+                    m.next = "WB_ENABLE_1"
 
             with m.State("WB_ENABLE_1"):
                 wb_write()
@@ -171,10 +142,14 @@ class Sequencer(Component):
                     self.efb.adr.eq(0x70)
                 ]
 
-                next_state_if_asserted(self.efb.ack, "WB_ENABLE_2")
+                with m.If(self.efb.ack):
+                    m.next = "WB_ENABLE_2"
 
             with m.State("WB_ENABLE_2"):
-                m.d.comb += self.efb.adr.eq(0x70)
+                m.d.comb += [
+                    self.efb.dat_w.eq(0x80),
+                    self.efb.adr.eq(0x70)
+                ]
 
                 m.next = "WB_CMD_1"
 
@@ -185,7 +160,8 @@ class Sequencer(Component):
                     self.efb.adr.eq(0x71)
                 ]
 
-                next_state_if_asserted(self.efb.ack, "WB_CMD_2")
+                with m.If(self.efb.ack):
+                    m.next = "WB_CMD_2"
 
             with m.State("WB_CMD_2"):
                 m.d.comb += [
@@ -202,14 +178,21 @@ class Sequencer(Component):
 
             with m.State("WB_OPERAND_1"):
                 wb_write()
-                wb_data_slice_ops()
-                m.d.comb += self.efb.adr.eq(0x71)
+                m.d.comb += [
+                    self.efb.dat_w.eq(
+                        self.ctl.cmd.ops.as_value().word_select(op_idx, 8)),
+                    self.efb.adr.eq(0x71)
+                ]
 
-                next_state_if_asserted(self.efb.ack, "WB_OPERAND_2")
+                with m.If(self.efb.ack):
+                    m.next = "WB_OPERAND_2"
 
             with m.State("WB_OPERAND_2"):
-                wb_data_slice_ops()
-                m.d.comb += self.efb.adr.eq(0x71)
+                m.d.comb += [
+                    self.efb.dat_w.eq(
+                        self.ctl.cmd.ops.as_value().word_select(op_idx, 8)),
+                    self.efb.adr.eq(0x71)
+                ]
 
                 with m.If(curr_op < self.ctl.op_len - 1):
                     m.d.sync += curr_op.eq(curr_op + 1)
@@ -224,7 +207,8 @@ class Sequencer(Component):
                     m.next = "WB_DISABLE_1"
 
             with m.State("WB_DATA_1"):
-                wb_data_slice_data()
+                m.d.comb += self.efb.dat_w.eq(
+                    self.ctl.wr.data.as_value().word_select(data_idx, 8))
                 with m.If(self.ctl.xfer_is_wr):
                     wb_write()
                     m.d.comb += self.efb.adr.eq(0x71)
@@ -235,15 +219,19 @@ class Sequencer(Component):
                 with m.If(self.efb.ack):
                     m.d.sync += self.ctl.rd.data.eq(self.efb.dat_r)
 
-                next_state_if_asserted(self.efb.ack, "WB_DATA_2")
+                with m.If(self.efb.ack):
+                    m.next = "WB_DATA_2"
 
             with m.State("WB_DATA_2"):
-                wb_data_slice_data()
+                m.d.comb += self.efb.dat_w.eq(
+                    self.ctl.wr.data.as_value().word_select(data_idx, 8))
                 with m.If(self.ctl.xfer_is_wr):
                     m.d.comb += self.efb.adr.eq(0x71)
                 with m.Else():
-                    m.d.comb += self.efb.adr.eq(0x73)
-                m.d.comb += self.ctl.rd.stb.eq(1)
+                    m.d.comb += [
+                        self.efb.adr.eq(0x73),
+                        self.ctl.rd.stb.eq(1)
+                    ]
 
                 with m.If(curr_data < self.ctl.data_len - 1):
                     m.d.sync += curr_data.eq(curr_data + 1)
@@ -259,13 +247,15 @@ class Sequencer(Component):
                 wb_write()
                 m.d.comb += self.efb.adr.eq(0x70)
 
-                next_state_if_asserted(self.efb.ack, "WB_DISABLE_2")
+                with m.If(self.efb.ack):
+                    m.next = "WB_DISABLE_2"
 
             with m.State("WB_DISABLE_2"):
                 m.next = "IDLE"
                 m.d.comb += self.efb.adr.eq(0x70)
                 m.d.comb += self.ctl.done.eq(1)
 
-                next_state_if_asserted(self.ctl.req, "WB_ENABLE_1")
+                with m.If(self.efb.ack):
+                    m.next = "WB_ENABLE_1"
 
         return m
